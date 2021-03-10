3bca1605a662fcf29bd70e281f67aaeeaf799b9f
Gilleece <seangilleece@gmail.com>
2021-03-10 18:47:29 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    $('.sidenav').sidenav({ edge: "right" });
-});
-
-$(document).ready(function () {
-    $('.collapsible').collapsible();
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
 });
